737dcf2319679e18853f1c8b1c0bf06b182082ea
Ivan <Shibkikh.I.V@sberbank.ru>
2026-05-24 13:33:45 +0300

    mtls log

--- internal/transport/transport.go
+++ internal/transport/transport.go
@@ -15,14 +15,17 @@ import (
 func Listen(ctx context.Context, listener net.Listener, gate4Server *Gate4Server, tlsConfig *tls.Config) error {
 
 	opts := []grpc.ServerOption{}
+	mtls := "disabled"
 	if tlsConfig != nil {
+		mtls = "enabled"
 		opts = append(opts, grpc.Creds(credentials.NewTLS(tlsConfig)))
 	}
 
 	grpcServer := grpc.NewServer(opts...)
 
 	pb.RegisterGate4Server(grpcServer, gate4Server)
-	slog.Info("grpc server is ready to serve requests", "network", listener.Addr().Network(), "address", listener.Addr().String())
+
+	slog.Info("grpc server is ready to serve requests", "network", listener.Addr().Network(), "address", listener.Addr().String(), "mtls", mtls)
 
 	errorc := make(chan error, 1)
 	go func() {
