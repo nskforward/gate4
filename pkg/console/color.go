42ef01d8c6d27b02b74ae09f772889afbd01b805
Ivan <Shibkikh.I.V@sberbank.ru>
2026-05-28 22:32:27 +0300

    increase performance x2 of console logger

--- pkg/console/color.go
+++ pkg/console/color.go
@@ -1,5 +1,7 @@
 package console
 
+import "fmt"
+
 type Color struct {
 	R int
 	G int
@@ -18,3 +20,14 @@ var (
 	Gray100 = Color{100, 100, 100}
 	Gray200 = Color{200, 200, 200}
 )
+
+func (color Color) String() string {
+	return fmt.Sprintf("38;5;%d", rgbTo256(color.R, color.G, color.B))
+}
+
+func rgbTo256(r, g, b int) int {
+	r6 := r * 5 / 255
+	g6 := g * 5 / 255
+	b6 := b * 5 / 255
+	return 16 + 36*r6 + 6*g6 + b6
+}
--- pkg/console/slog.go
+++ pkg/console/slog.go
@@ -1,12 +1,33 @@
 package console
 
 import (
+	"bytes"
 	"context"
-	"fmt"
 	"io"
 	"log/slog"
 	"os"
+	"strconv"
 	"strings"
+	"sync"
+)
+
+var (
+	logDebugStr = FormatText("DEBUG", Black, Bold)
+	logInfoStr  = FormatText("INFO", Green, Bold)
+	logWarnStr  = FormatText("WARN", Yellow, Bold)
+	logErrorStr = FormatText("ERROR", Red, Bold)
+
+	logTimePrefix = buildPrefix(Blue)
+	logMsgPrefix  = buildPrefix(White, Bold)
+	logKeyPrefix  = buildPrefix(Gray100)
+)
+
+var (
+	bufferPool = sync.Pool{
+		New: func() any {
+			return bytes.NewBuffer(make([]byte, 2048))
+		},
+	}
 )
 
 type LoggerHandler struct {
@@ -31,8 +52,8 @@ func (h *LoggerHandler) Enabled(_ context.Context, level slog.Level) bool {
 }
 
 func (h *LoggerHandler) WithAttrs(attrs []slog.Attr) slog.Handler {
-	newAttrs := make([]slog.Attr, 0, len(h.attrs)+len(attrs))
-	newAttrs = append(newAttrs, h.attrs...)
+	newAttrs := make([]slog.Attr, len(h.attrs), len(h.attrs)+1)
+	copy(newAttrs, h.attrs)
 	if h.group != "" {
 		newAttrs = append(newAttrs, slog.Attr{
 			Key:   h.group,
@@ -41,7 +62,6 @@ func (h *LoggerHandler) WithAttrs(attrs []slog.Attr) slog.Handler {
 	} else {
 		newAttrs = append(newAttrs, attrs...)
 	}
-
 	return &LoggerHandler{w: h.w, wd: h.wd, level: h.level, attrs: newAttrs}
 }
 
@@ -50,16 +70,28 @@ func (h *LoggerHandler) WithGroup(name string) slog.Handler {
 }
 
 func (h *LoggerHandler) Handle(ctx context.Context, r slog.Record) error {
-	fmt.Fprintln(h.w,
-		FormatText(r.Time.Format(timeFormat), Blue),
-		formatLevel(r.Level),
-		FormatText(r.Message, White, Bold),
-	)
+	buf := bufferPool.Get().(*bytes.Buffer)
+	defer bufferPool.Put(buf)
+	buf.Reset()
+
+	buf.WriteString(logTimePrefix)
+	buf.WriteString(r.Time.Format(timeFormat))
+	buf.WriteString(resetCode)
+	buf.WriteByte(' ')
+	buf.WriteString(formatLevel(r.Level))
+	buf.WriteByte(' ')
+	buf.WriteString(logMsgPrefix)
+	buf.WriteString(r.Message)
+	buf.WriteString(resetCode)
+	buf.WriteByte('\n')
 
 	attrs := make([]slog.Attr, 0, len(h.attrs)+r.NumAttrs()+1)
+	file := r.Source().File
+	file = strings.TrimPrefix(file, h.wd)
+	src := file + ":" + strconv.Itoa(r.Source().Line)
 	attrs = append(attrs, slog.Attr{
 		Key:   "src",
-		Value: slog.StringValue(fmt.Sprintf("%s:%d", strings.TrimPrefix(r.Source().File, h.wd), r.Source().Line)),
+		Value: slog.StringValue(src),
 	})
 	attrs = append(attrs, h.attrs...)
 	r.Attrs(func(a slog.Attr) bool {
@@ -67,49 +99,58 @@ func (h *LoggerHandler) Handle(ctx context.Context, r slog.Record) error {
 		return true
 	})
 
-	printAttrs(attrs, 1)
-	fmt.Println()
+	writeAttrs(buf, attrs, 1)
+	buf.WriteByte('\n')
 
-	return nil
+	_, err := h.w.Write(buf.Bytes())
+	return err
 }
 
-func printAttrs(attrs []slog.Attr, padding int) {
+func writeAttrs(buf *bytes.Buffer, attrs []slog.Attr, padding int) {
 	maxKey := 0
 	for _, a := range attrs {
 		if len(a.Key) > maxKey {
 			maxKey = len(a.Key)
 		}
 	}
-	for _, a := range attrs {
-		fmt.Print(strings.Repeat("    ", padding), FormatText(fmt.Sprintf("%s:", a.Key), Gray100), strings.Repeat(" ", maxKey-len(a.Key)+1))
-		printValue(a.Value, padding)
-	}
-}
 
-func printValue(v slog.Value, padding int) {
-	if v.Kind() == slog.KindString {
-		//fmt.Print("\"", v.String(), "\"\n")
-		fmt.Print(v.String(), "\n")
-		return
+ATTR_RANGE:
+	for _, a := range attrs {
+		for range padding {
+			buf.WriteString("    ")
+		}
+		buf.WriteString(logKeyPrefix)
+		buf.WriteString(a.Key)
+		buf.WriteByte(':')
+		buf.WriteString(resetCode)
+		for range maxKey - len(a.Key) + 1 {
+			buf.WriteByte(' ')
+		}
+
+		switch a.Value.Kind() {
+		case slog.KindString:
+			buf.WriteString(a.Value.String())
+		case slog.KindGroup:
+			buf.WriteByte('\n')
+			writeAttrs(buf, a.Value.Group(), padding+1)
+			continue ATTR_RANGE
+		default:
+			buf.WriteString(a.Value.String())
+		}
+		buf.WriteByte('\n')
 	}
-	if v.Kind() == slog.KindGroup {
-		fmt.Println()
-		printAttrs(v.Group(), padding+1)
-		return
-	}
-	fmt.Print(v.String(), "\n")
 }
 
 func formatLevel(level slog.Level) string {
 	switch level {
 	case slog.LevelDebug:
-		return FormatText("DEBUG", Black, Bold)
+		return logDebugStr
 	case slog.LevelInfo:
-		return FormatText("INFO", Green, Bold)
+		return logInfoStr
 	case slog.LevelWarn:
-		return FormatText("WARN", Yellow, Bold)
+		return logWarnStr
 	case slog.LevelError:
-		return FormatText("ERROR", Red, Bold)
+		return logErrorStr
 	default:
 		return level.String()
 	}
